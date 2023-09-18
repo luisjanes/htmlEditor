0e78b870d60fdaeba319cd9c6b5772f705d7e8d5
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2023-09-18 16:03:37 -0300

    Adicionar arquivos de projeto.

---
+++ Menu.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace htmlEditor
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Carai borracha");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace htmlEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+
+        }
+    }
+}
